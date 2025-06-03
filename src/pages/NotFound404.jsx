e29f6e1ffdce17fb19cead13b38ae614f0507ff0
Emmanuel Adetoro <112830359+ademto@users.noreply.github.com>
2025-06-02 21:24:36 -0400

    404 page

--- src/pages/NotFound404.jsx
+++ src/pages/NotFound404.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'; // remove if not using react-router
 
 const NotFound404 = () => {
   return (
-    <div className="">
-      <h1>Oops! Page Not Found</h1>
-      <p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
-      <Link className='bg-black text-white' to="/">Go to Homepage</Link>
+    <div className="flex flex-col items-center justify-centertext-center mt-20">
+      <h1 className='text-2xl font-bold'>Oops! Page Not Found</h1>
+      <p className='pt-2'>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
+      <Link className='inline-block mt-10 px-4 py-2 rounded-lg bg-black text-white' to="/">Go to Homepage</Link>
     </div>
   );
 };
